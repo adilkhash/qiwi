6b979bcada238065febefbddc30abdec722cf8c9
adilkhash <adil@playrix.com>
2019-06-03 10:33:09 +0600

    added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+install_requires = [
+    'requests==2.22',
+    'pytz==2019.1',
+    'python-dateutil==2.8.0',
+]
+
+setup(
+    name='qiwi',
+    version='0.1',
+    packages=['qiwi_payments'],
+    url='https://github.com/adilkhash/qiwi',
+    license='MIT',
+    author='Adylzhan Khashtamov',
+    author_email='adil.khashtamov@gmail.com',
+    description='Qiwi API client',
+    install_requires=install_requires,
+)
